76f5083a973506e5a4cf980d678be48d3240f5f2
YosefhDyn <91265403+YosefhDyn@users.noreply.github.com>
2025-09-25 23:53:47 -0500

    Update indexer.py

--- indexer.py
+++ indexer.py
@@ -1,12 +1,12 @@
 import re
 
-# Función para indexar palabras clave a cursos
+# funcion para indexar palabras clave a cursos :|
 def index_course(index, text, course_id):
-    words = re.findall(r'\b\w+\b', text.lower())  # Buscar palabras alfanuméricas
+    words = re.findall(r'\b\w+\b', text.lower())  # buscar palabras alfanumericas :)
     for word in words:
-        if len(word) <= 2:  # Ignorar palabras muy cortas
+        if len(word) <= 2:  # ignorar palabras muy cortas :/
             continue
         if word not in index:
             index[word] = []
         if course_id not in index[word]:
-            index[word].append(course_id)
+            index[word].append(course_id)  # agregar el curso al indice :)
